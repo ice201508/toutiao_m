30bcf165a1959bc2917642225de8d2f7d7ab3d72
ice201508 <2235907780@qq.com>
2020-07-02 11:52:30 +0800

    频道列表样式的调整，设计到插槽，伪元素的修改

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <!-- 顶部导航 -->
     <van-nav-bar left-arrow @click-right="onClickRight" class="home-nav-bar">
       <div slot="left" class="navbar-left">
         <img :src="require('@/assets/logo.png')" alt="" />
@@ -13,12 +14,30 @@
         >
       </div>
     </van-nav-bar>
+    <!-- 频道列表 -->
+    <van-tabs class="home-tabs" v-model="active" animated swipeable>
+      <van-tab title="标签 1">内容 1</van-tab>
+      <van-tab title="标签 2">内容 2</van-tab>
+      <van-tab title="标签 3">内容 3</van-tab>
+      <van-tab title="标签 4">内容 4</van-tab>
+      <van-tab title="标签 5">内容 5</van-tab>
+      <van-tab title="标签 6">内容 6</van-tab>
+      <div slot="nav-right" class="placeholder"></div>
+      <div slot="nav-right" class="ht-icon">
+        <i class="iconfont icongengduo"></i>
+      </div>
+    </van-tabs>
   </div>
 </template>
 
 <script>
 export default {
   name: 'home',
+  data() {
+    return {
+      active: 1
+    };
+  },
   methods: {
     onClickRight() {
       console.log(123);
@@ -49,6 +68,35 @@ export default {
       .van-icon {
         color: #fff;
       }
+    }
+  }
+}
+.home-tabs {
+  .placeholder {
+    flex-shrink: 0;
+    width: 66px;
+    height: 88px;
+  }
+  .ht-icon {
+    width: 66px;
+    height: 88px;
+    position: fixed;
+    right: 0;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: rgba(255, 255, 255, 0.9);
+    i {
+      font-size: 33px;
+    }
+    &:before {
+      content: '';
+      position: absolute;
+      left: 0;
+      width: 1px;
+      height: 58px;
+      background: url('~@/assets/gradient-gray-line.png') no-repeat;
+      background-size: contain;
     }
   }
 }
